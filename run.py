371a4395e8db6811e9c31a7155a3742a84836560
Andrew Babenko <ababenk@softserveinc.com>
2020-03-26 15:18:15 +0200

    Added class to perform processing of bouquet design data

--- run.py
+++ run.py
@@ -1,3 +1,36 @@
+class BouquetDesign:
+    def __init__(self, design):
+        self.design = design
+        self.bouquet_name = design[0]
+        self.bouquet_size = design[1]
+        self.total_flowers_in_bouquet = BouquetDesign.get_total_flowers_in_bouquet(design)
+        self.flowers = BouquetDesign.get_flowers(design[2:len(design) - len(str(self.total_flowers_in_bouquet))])
+
+    @staticmethod
+    def get_flowers(flowers_quantity_string: str):
+        flowers = {}
+
+        def _get_flower_species(bouquet_item: str):
+            return bouquet_item.isalpha()
+
+        flower_species = [*filter(_get_flower_species, flowers_quantity_string)]
+        for flower_specie in flower_species:
+            flowers[flower_specie], flowers_quantity_string = flowers_quantity_string.split(flower_specie)
+            flowers[flower_specie] = int(flowers[flower_specie])
+
+        return flowers
+
+    @staticmethod
+    def get_total_flowers_in_bouquet(bouquet_design: str):
+        total_quantity = ''
+        for index in range(len(bouquet_design) - 1, 0, -1):
+            if bouquet_design[index].isnumeric():
+                total_quantity = f"{bouquet_design[index]}{total_quantity}"
+            else:
+                break
+        return int(total_quantity)
+
+
 def main():
     """ Main function, perform action once script runs
     """
@@ -8,7 +41,7 @@ def main():
         design = str(input())
         if design == "":
             break
-        bouquet_designs.append(design)
+        bouquet_designs.append(BouquetDesign(design))
 
     print('Please, enter flowers. Once you finish, enter blank line for proceeding to the next step')
     flowers_storage = []
