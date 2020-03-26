3bacd8f16524d3093acb3b6ff84ed47a40fa1bb2
Andrew Babenko <ababenk@softserveinc.com>
2020-03-26 16:43:54 +0200

    Created class for bouquets creation

--- run.py
+++ run.py
@@ -60,6 +60,60 @@ class BouquetDesign:
         return int(total_quantity)
 
 
+class Bouquet:
+    def __init__(self, design: BouquetDesign, flowers_storage: FlowersStorage):
+        self.design = design
+        self.flowers_storage = flowers_storage
+        self.bouquet = defaultdict(int)
+
+    def create(self):
+        available_flowers = getattr(self.flowers_storage, self.design.bouquet_size)
+        amount_of_used_flowers = 0
+
+        for flower_specie, flowers_amount in self.design.flowers.items():
+            if self.flowers_storage.is_amount_available(self.design.bouquet_size, flower_specie, flowers_amount):
+                self.bouquet[flower_specie] = flowers_amount
+                self.flowers_storage.reduce_amount(self.design.bouquet_size, flower_specie, flowers_amount)
+                amount_of_used_flowers += flowers_amount
+
+            else:
+                return False, (f"Unable create a bouquet by design {self.design.design}: available amount of "
+                               f"flower specie {flower_specie} is not enough. Available amount of "
+                               f"specie {flower_specie} is {available_flowers[flower_specie]}, "
+                               f"required {flowers_amount}")
+
+        if amount_of_used_flowers == self.design.total_flowers_in_bouquet:
+            pass
+        elif amount_of_used_flowers < self.design.total_flowers_in_bouquet:
+            self._fill_available_slots(self.design.total_flowers_in_bouquet - amount_of_used_flowers)
+        else:
+            raise ValueError("Unexpected number of used flowers: amount of used flowers in bouquet is bigger "
+                             "than required total number of flowers by bouquet design")
+
+    def _fill_available_slots(self, slots):
+        available_flowers = getattr(self.flowers_storage, self.design.bouquet_size)
+
+        available_flowers_copy = available_flowers.copy()
+        for flower_specie, flowers_amount in available_flowers_copy.items():
+            if flowers_amount >= slots:
+                self.bouquet[flower_specie] += slots
+                self.flowers_storage.reduce_amount(self.design.bouquet_size, flower_specie, slots)
+                slots -= slots
+                break
+            else:
+                self.bouquet[flower_specie] += flowers_amount
+                slots -= flowers_amount
+                self.flowers_storage.reduce_amount(self.design.bouquet_size, flower_specie, flowers_amount)
+
+        if slots > 0:
+            raise ValueError(f'Not enough flowers of size {self.design.bouquet_size} to create the bouquet')
+
+    def __str__(self):
+        return f'{self.design.bouquet_name}{self.design.bouquet_size}' + ''.join(
+            f'{amount}{specie}' for specie, amount in self.bouquet.items()
+        )
+
+
 def main():
     """ Main function, perform action once script runs
     """
@@ -81,8 +135,12 @@ def main():
 
         flowers_storage.append(flower)
 
-    print(bouquet_designs)
-    print(flowers_storage)
+    bouquets = []
+    for bouquet_design in bouquet_designs:
+        bouquet = Bouquet(bouquet_design, flowers_storage)
+        bouquet.create()
+        bouquets.append(bouquet)
+        print(str(bouquet))
 
 
 if __name__ == '__main__':
